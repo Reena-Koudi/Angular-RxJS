6a532bb07789f390a4e78ede5e6690c9f7f46784
Reena Koudi <reena.koudi@holition.com>
2020-01-07 15:45:10 +0000

    working with async pipe

---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Product } from "./product";
+import { ProductService } from "./product.service";
+import { Observable } from "rxjs";
+
+@Component({
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
+})
+export class ProductListComponent implements OnInit {
+  pageTitle = "Product List";
+  errorMessage = "";
+  categories;
+
+  products$: Observable<Product[]>;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts();
+  }
+
+  onAdd(): void {
+    console.log("Not yet implemented");
+  }
+
+  onSelected(categoryId: string): void {
+    console.log("Not yet implemented");
+  }
+}
