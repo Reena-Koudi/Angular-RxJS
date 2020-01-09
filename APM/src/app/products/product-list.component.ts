2feaf2792db0e99e771a3c6d97b257eab25bfe46
Reena Koudi <reena.koudi@holition.com>
2020-01-09 11:38:24 +0000

    Filtering a stream: Demo I

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 import { ProductService } from "./product.service";
 import { EMPTY } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { catchError, map } from "rxjs/operators";
 
 @Component({
   templateUrl: "./product-list.component.html",
@@ -12,12 +12,23 @@ export class ProductListComponent {
   pageTitle = "Product List";
   errorMessage = "";
   categories;
+  selectedCategoryId = 1;
 
   products$ = this.productService.productsWithCategory$.pipe(
     catchError(err => {
       this.errorMessage = err;
       return EMPTY;
     })
+  );
+
+  productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+    map(products =>
+      products.filter(product =>
+        this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true
+      )
+    )
   );
 
   constructor(private productService: ProductService) {}
